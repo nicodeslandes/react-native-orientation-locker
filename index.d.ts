998839ef4cdf22a6c73b5e059dd642934d696b5a
Nicolas Deslandes <ndeslandes@live.fr>
2019-01-20 22:38:17 +0000

    Update Typescript definitions with new API for addOrientationListener/addDeviceOrientationListener

--- index.d.ts
+++ index.d.ts
@@ -10,8 +10,10 @@ declare module 'react-native-orientation-locker' {
   namespace Orientation {
     type Orientation = "PORTRAIT" | "PORTRAIT-UPSIDEDOWN" | "LANDSCAPE-LEFT" | "LANDSCAPE-RIGHT" | "UNKNOWN";
   
-    export function addOrientationListener(callback: (orientation: Orientation, deviceOrientation: Orientation) => void): void;
-    export function removeOrientationListener(callback: (orientation: Orientation, deviceOrientation: Orientation) => void): void;
+    export function addOrientationListener(callback: (orientation: Orientation) => void): void;
+    export function removeOrientationListener(callback: (orientation: Orientation) => void): void;
+    export function addDeviceOrientationListener(callback: (deviceOrientation: Orientation) => void): void;
+    export function removeDeviceOrientationListener(callback: (deviceOrientation: Orientation) => void): void;
   
     export function getInitialOrientation(): Orientation;
     export function lockToPortrait(): void;
